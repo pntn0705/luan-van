fb31de3813fdd690ea1c0ffd7fe6cca68d919496
pntn0705 <pntn0705playgame@gmail.com>
2021-03-27 10:13:01 +0700

    change setting

--- app/settings.py
+++ app/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(str(BASE_DIR), 'assets'),
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(str(BASE_DIR), 'media')
